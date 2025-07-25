26585389f8ddbeb12b67b0c6c321f273b78025a7
Abbasi <usamahafeez.abbasi1234@gmail.com>
2025-07-25 06:32:48 +0500

    Added the documentation to driver.py

--- scrapper/driver.py
+++ scrapper/driver.py
@@ -2,11 +2,20 @@ import undetected_chromedriver as uc
 from selenium.webdriver.chrome.options import Options
 
 def get_driver(headless=False):
+    """
+    Launches a new Chrome browser instance using undetected_chromedriver.
+    
+    Args:
+        headless (bool): Whether to run the browser in headless (invisible) mode.
+
+    Returns:
+        WebDriver: A Selenium WebDriver object to control the browser.
+    """
     options = Options()
     if headless:
-        options.add_argument("--headless")
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--headless") # Run browser invisibly
+    options.add_argument("--start-maximized") # Open in full screen
+    options.add_argument("--disable-blink-features=AutomationControlled")  # Hide automation
 
-    driver = uc.Chrome(options=options)
+    driver = uc.Chrome(options=options) # Launch browser
     return driver
